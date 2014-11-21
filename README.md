f6be0860436bb2047a00348642cbca782a05a18f
Jesse Clark <jjclark1982@gmail.com>
2014-11-21 21:49:21 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
 metalsmith-pdf
 ==============
 
-pdf generator for metalsmith
+PDF generator for metalsmith
+
+## Installation
+
+[Install wkhtmltopdf](http://wkhtmltopdf.org/downloads.html) on your system.
+
+And install this plugin:
+
+    $ npm install metalsmith-pdf
+
+## Usage
+
+All options other than `pattern` are passed directly to the [wkhtmltopdf options parser](http://wkhtmltopdf.org/usage/wkhtmltopdf.txt)
+
+```JavaScript
+var pdf = require('metalsmith-pdf');
+
+metalsmith.use(pdf({
+  pattern: "**/*.html",
+  printMediaType: true,
+  pageSize: "letter"
+}));
+```
+
+## CLI Usage
+
+All of the same options apply, just add them to the `"plugins"` key in your `metalsmith.json` configuration:
+
+```JSON
+{
+  "plugins": {
+    "metalsmith-pdf": {
+      "pattern": "**/*.html",
+      "printMediaType": true,
+      "pageSize": "letter"
+    }
+  }
+}
+```
+
+## License
+
+  MIT
+
+## TODO
+
+- Support absolute links
---
+++ index.js
@@ -0,0 +1,49 @@
+var wkhtmltopdf = require("wkhtmltopdf");
+var async = require("async");
+var minimatch = require('minimatch');
+
+var defaults = {
+    pattern: "**/*.html",
+    printMediaType: true,
+    pageSize: "letter"
+};
+
+module.exports = function(options){
+    if (typeof options === 'string') {
+        options = {pattern: options};
+    }
+    if (typeof options !== 'object') {
+        options = {};
+    }
+    for (var key in defaults) {
+        if (Object.hasOwnProperty.call(options, key)) {
+            options[key] = defaults[key];
+        }
+    }
+    delete options.output;
+    var pattern = options.pattern || "**/*.html";
+    delete options.pattern;
+
+    return function(files, metalsmith, done){
+        var paths = Object.keys(files).filter(minimatch.filter(pattern))
+        async.each(paths, makePdf.bind(null, options, files), done);
+    }
+}
+
+function makePdf(options, files, path, done) {
+    var sourceFile = files[path];
+
+    var stream = wkhtmltopdf(sourceFile.contents, options);
+    var buffers = []
+    stream.on('data', function(part){
+        buffers.push(part)
+    });
+    stream.on('end', function(){
+        var destPath = path.replace(/[^\.]*$/i, 'pdf');
+        var destFile = {
+            contents: Buffer.concat(buffers)
+        }
+        files[destPath] = destFile;
+        done();
+    });
+}
